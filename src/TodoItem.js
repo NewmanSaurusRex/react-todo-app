a75a4ac4f1703f18104e5c9bd9a2089cca48e590
MNewman <megan.m.boggs@gmail.com>
2022-07-17 16:51:45 -0700

    fixed bug in todoitem.js
    items not rendering appropriately due to this.setState -- updated to class component to fix bug

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,26 +1,43 @@
 import React from "react";
 import styles from "./TodoItem.module.css";
 
-export default function TodoItem(props) {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
   };
 
-  const { completed, id, title } = props.todo;
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
 
-  return (
-    <li className={styles.item}>
-      <input
-        type="checkbox"
-        className={styles.checkbox}
-        checked={completed}
-        onChange={() => props.handleChangeProps(id)}
-      />
-      <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-      <span style={completed ? completedStyle : null}>{title}</span>
-    </li>
-  );
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
+
+    const { completed, id, title } = this.props.todo;
+
+    return (
+      <li className={styles.item}>
+        <div>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
+        <input type="text" className={styles.textInput} />
+      </li>
+    );
+  }
 }
+
+export default TodoItem;
